69714498556f7ddc8ccaea985083c9446ba7c4b5
Roussetos Karafyllakis <84732055+gene-roussetos@users.noreply.github.com>
2023-02-07 00:43:34 +0000

    test failed response

--- tests/Unit/GraphQlSdk/RefundCreateTest.php
+++ tests/Unit/GraphQlSdk/RefundCreateTest.php
@@ -8,7 +8,6 @@ use PHPUnit\Framework\TestCase;
 use Rvvup\Sdk\Curl;
 use Rvvup\Sdk\Factories\Inputs\RefundCreateInputFactory;
 use Rvvup\Sdk\GraphQlSdk;
-use Rvvup\Sdk\Inputs\RefundCreateInput;
 use Rvvup\Sdk\Response;
 
 /**
@@ -17,12 +16,25 @@ use Rvvup\Sdk\Response;
  */
 class RefundCreateTest extends TestCase
 {
+    private $refundCreateData = [
+        'data' => [
+            'refundCreate' => [
+                'id' => 'REXXXXXXX',
+                'amount' => [
+                    'amount' => 10.00,
+                    'currency' => 'GBP'
+                ],
+                'status' => 'SUCCESSFUL'
+            ]
+        ]
+    ];
+
     /**
      * @test
      * @group refund
      *
      * @return void
-     * @throws \Exception
+     * @throws \JsonException
      */
     public function assert_successful_refund_call(): void
     {
@@ -38,20 +50,7 @@ class RefundCreateTest extends TestCase
             false
         );
 
-        $data = [
-            'data' => [
-                'refundCreate' => [
-                    'id' => 'REXXXXXXX',
-                    'amount' => [
-                        'amount' => 10.00,
-                        'currency' => 'GBP'
-                    ],
-                    'status' => 'SUCCESSFUL'
-                ]
-            ]
-        ];
-
-        $response = new Response(200, json_encode($data, JSON_THROW_ON_ERROR), []);
+        $response = new Response(200, json_encode($this->refundCreateData, JSON_THROW_ON_ERROR), []);
 
         $curlStub->method('request')->willReturn($response);
 
@@ -59,7 +58,79 @@ class RefundCreateTest extends TestCase
 
         $this->assertIsArray($graphQlSdk->refundCreate($inputFactory->create(
             'ORXXXX',
-            '0000',
+            '10.00',
+            'GBP',
+            'KEY',
+            'RANDOM REASON'
+        )));
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     * @throws \JsonException
+     */
+    public function assert_false_on_empty_response(): void
+    {
+        $curlStub = $this->createStub(Curl::class);
+
+        $graphQlSdk = new GraphQlSdk(
+            'https://endpoint.com/url',
+            'MEXXXXXXX',
+            'AUTH_TOKEN',
+            'USER_AGENT',
+            $curlStub,
+            null,
+            false
+        );
+
+        $response = new Response(200, json_encode($this->refundCreateData['data'], JSON_THROW_ON_ERROR), []);
+
+        $curlStub->method('request')->willReturn($response);
+
+        $inputFactory = new RefundCreateInputFactory();
+
+        $this->assertFalse($graphQlSdk->refundCreate($inputFactory->create(
+            'ORXXXX',
+            '10.00',
+            'GBP',
+            'KEY',
+            'RANDOM REASON'
+        )));
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     * @throws \JsonException
+     * @throws \Exception
+     */
+    public function assert_exception_on_non_2xx_response_code(): void
+    {
+        $this->expectException(\Exception::class);
+        $curlStub = $this->createStub(Curl::class);
+
+        $graphQlSdk = new GraphQlSdk(
+            'https://endpoint.com/url',
+            'MEXXXXXXX',
+            'AUTH_TOKEN',
+            'USER_AGENT',
+            $curlStub,
+            null,
+            false
+        );
+
+        $response = new Response(400, json_encode($this->refundCreateData, JSON_THROW_ON_ERROR), []);
+
+        $curlStub->method('request')->willReturn($response);
+
+        $inputFactory = new RefundCreateInputFactory();
+
+        $this->assertFalse($graphQlSdk->refundCreate($inputFactory->create(
+            'ORXXXX',
+            '10.00',
             'GBP',
             'KEY',
             'RANDOM REASON'
