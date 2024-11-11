83ff9940e1fcbdd5eb0d56a7aa715d52e4c06797
Aashwin Mohan <aashwin@rvvup.com>
2024-11-11 13:52:16 +0000

    Add Checkout Templates Rest API

---
+++ src/Rest/CheckoutTemplates.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+
+namespace Rvvup\Sdk\Rest;
+
+use Rvvup\Api\CheckoutTemplatesApi;
+use Rvvup\Api\Model\CheckoutTemplate;
+use Rvvup\Api\Model\CheckoutTemplateCreateInput;
+use Rvvup\Api\Model\CheckoutTemplatePage;
+use Rvvup\Api\Model\CheckoutTemplateUpdateInput;
+use Rvvup\ApiException;
+
+class CheckoutTemplates
+{
+    /**
+     * @var RvvupClient
+     */
+    private $client;
+
+    /**
+     * @var CheckoutTemplatesApi
+     */
+    private $api;
+
+    public function __construct(RvvupClient $client)
+    {
+        $this->client = $client;
+        $this->api = new CheckoutTemplatesApi(null, $client->configuration());
+    }
+
+    /**
+     * @param CheckoutTemplateCreateInput $input
+     * @param string|null $idempotencyKey
+     * @return CheckoutTemplate
+     * @throws ApiException
+     */
+    public function create(CheckoutTemplateCreateInput $input, ?string $idempotencyKey): CheckoutTemplate
+    {
+        return $this->api->createCheckoutTemplate($this->client->getMerchantId(), $input, $idempotencyKey);
+    }
+
+    /**
+     * @param string $id
+     * @param CheckoutTemplateUpdateInput $input
+     * @return CheckoutTemplate
+     * @throws ApiException
+     */
+    public function update(string $id, CheckoutTemplateUpdateInput $input): CheckoutTemplate
+    {
+        return $this->api->updateCheckoutTemplate($id, $this->client->getMerchantId(), $input);
+    }
+
+    /**
+     * @param string $id
+     * @return CheckoutTemplate
+     * @throws ApiException
+     */
+    public function get(string $id): CheckoutTemplate
+    {
+        return $this->api->getCheckoutTemplate($id, $this->client->getMerchantId());
+    }
+
+    /**
+     * @param string $offset
+     * @param string $limit
+     * @return CheckoutTemplatePage
+     * @throws ApiException
+     */
+    public function list(string $offset, string $limit): CheckoutTemplatePage
+    {
+        return $this->api->listCheckoutTemplates($this->client->getMerchantId(), $offset, $limit);
+    }
+}
